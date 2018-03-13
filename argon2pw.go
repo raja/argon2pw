6a1937e06451301b110dad24d829154023289902
Raja Bhatia <raja@thehiring.co>
2018-03-13 11:30:52 -0400

    Update documentation for godoc

--- argon2pw.go
+++ argon2pw.go
@@ -3,24 +3,25 @@
 //
 // Example:
 // package main
-// import "github.com/raja/argon2pw"
+//  import "github.com/raja/argon2pw"
 //
-// func main() {
-// 	// Generate a hashed password
-// 	testPassword := `testPassword$x1w432b7^`
-// 	hashedPassword, err := argon2pw.GenerateSaltedHash(testPassword)
-// 	if err != nil {
-// 		log.Panicf("Hash generated returned error: %v", err)
-// 	}
+//  func main() {
+// 	 // Generate a hashed password
+// 	 testPassword := `testPassword$x1w432b7^`
+// 	 hashedPassword, err := argon2pw.GenerateSaltedHash(testPassword)
+// 	 if err != nil {
+// 	 	log.Panicf("Hash generated returned error: %v", err)
+// 	 }
 //
-// 	// Test correct password in constant time
-// 	valid, err := argon2pw.CompareHashWithPassword(hashedPassword, testPassword)
-// 	log.Printf("The password validity is %t against the hash", valid)
+// 	 // Test correct password in constant time
+// 	 valid, err := argon2pw.CompareHashWithPassword(hashedPassword, testPassword)
+// 	 log.Printf("The password validity is %t against the hash", valid)
 //
-// 	// Test incorrect password in constant time
-// 	valid, err = argon2pw.CompareHashWithPassword(hashedPassword, "badPass")
-// 	log.Printf("The password validity is %t against the hash", valid)
-// }
+// 	 // Test incorrect password in constant time
+// 	 valid, err = argon2pw.CompareHashWithPassword(hashedPassword, "badPass")
+// 	 log.Printf("The password validity is %t against the hash", valid)
+//  }
+//
 package argon2pw
 
 import (
